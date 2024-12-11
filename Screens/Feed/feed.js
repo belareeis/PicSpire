fbd858ed65e38c81bc318ee03c31e0dd443e4f1f
Ramon Souza <ramoginga43@gmail.com>
2024-12-11 19:59:13 -0300

    Criando popup criar

--- Screens/Feed/feed.js
+++ Screens/Feed/feed.js
@@ -1,6 +1,7 @@
 const button = document.getElementById("Criar");
 const modal = document.querySelector("dialog");
-const butttonClose = document.getElementById("btnFechar");
+
+
 
 button.onclick = function(){
     modal.showModal()
@@ -9,3 +10,34 @@ button.onclick = function(){
 butttonClose.onclick = function(){
     modal.close()
 }
+
+const openButton = document.getElementById("Criar");
+const popupDialog = document.getElementById("popupDialog");
+
+openButton.addEventListener("click", () => {
+    popupDialog.showModal();
+});
+
+
+const closeButton = document.getElementById("closePopup");
+
+closeButton.addEventListener("click", () => {
+    popupDialog.close();
+});
+
+
+const pictureInput = document.getElementById("picture__input");
+const previewImage = document.getElementById("preview-image");
+
+pictureInput.addEventListener("change", (event) => {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = (e) => {
+            previewImage.src = e.target.result; 
+            previewImage.style.display = "block"; 
+        };
+        reader.readAsDataURL(file); 
+        
+    }
+});
